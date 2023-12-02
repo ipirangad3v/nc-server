09109dadcd794b2a7af4a7bec0e39e1a7897ef72
Anthoni Ipiranga <anthoni.ipiranga@gmail.com>
2023-12-02 18:18:34 -0300

    removed firebase credentials

--- src/main/kotlin/com/thondigital/nc/application/auth/firebase/FirebaseAdmin.kt
+++ src/main/kotlin/com/thondigital/nc/application/auth/firebase/FirebaseAdmin.kt
@@ -3,16 +3,18 @@ package com.thondigital.nc.application.auth.firebase
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
-import java.io.InputStream
+import java.io.ByteArrayInputStream
+import java.util.Base64
 
 object FirebaseAdmin {
-    private val serviceAccount: InputStream? =
-        this::class.java.classLoader.getResourceAsStream("ktor-firebase-auth-firebase-adminsdk.json")
+    private val base64Config: String = System.getenv("GOOGLE_CONFIG_BASE64")
+    private val credentials: GoogleCredentials = GoogleCredentials.fromStream(
+        ByteArrayInputStream(Base64.getDecoder().decode(base64Config))
+    )
 
-    private val options: FirebaseOptions =
-        FirebaseOptions.builder()
-            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-            .build()
+    private val options: FirebaseOptions = FirebaseOptions.builder()
+        .setCredentials(credentials)
+        .build()
 
     fun init(): FirebaseApp = FirebaseApp.initializeApp(options)
 }
